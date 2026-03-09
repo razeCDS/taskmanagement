7eb579e3a2aeac9b8d9b6b1d396e6337da381619
Cezar Camargo da Silva <cezar.cds71@gmail.com>
2026-03-09 15:49:35 -0300

    [FEAT]: Configuração de extensions para ajuste de configurações do projeto

---
+++ src/TaskManagement.Application/Extensions/ServiceCollectionExtensiosn.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskManagement.Application.Extensions
+{
+    public static class ServiceCollectionExtensiosn
+    {
+        public static IServiceCollection AddApplicationConfiguration(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
---
+++ src/TaskManagement.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskManagement.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructureConfiguration(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
