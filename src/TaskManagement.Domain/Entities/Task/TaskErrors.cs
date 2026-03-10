bc0901ccf2e38d17d1652150effae3806b4675f9
Cezar Camargo da Silva <cezar.cds71@gmail.com>
2026-03-10 17:06:39 -0300

    [REFACTOR]: Alteração de mensagem de Error NotFound

--- src/TaskManagement.Domain/Entities/Task/TaskErrors.cs
+++ src/TaskManagement.Domain/Entities/Task/TaskErrors.cs
@@ -5,7 +5,7 @@ namespace TaskManagement.Domain.Entities.Task
 {
     public static class TaskErrors
     {
-        public static Error NotFound() => new("Task.NotFound", ErrorType.NotFound);
+        public static Error NotFound() => new("Task não encontrada.", ErrorType.NotFound);
         public static Error Unexpected(string message) => new(message, ErrorType.Unexpected);
         public static Error Validation(string message) => new(message, ErrorType.Validation);
     }
