14373af7cc0fae6e9b89d8708dae7276a975015e
Cezar Camargo da Silva <cezar.cds71@gmail.com>
2026-03-10 21:11:50 -0300

    [TEST]: Implementação de testes unitários para TaskService

---
+++ test/TaskManagement.Api.UnitTest/TaskManagement.Infrastructure/TaskRepositoryTest.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagement.Api.UnitTest.TaskManagement.Infrastructure
+{
+    public class TaskRepositoryTest
+    {
+        [Fact]
+        public void Test_Single()
+        {
+            Assert.True(true);
+        }
+    }
+}
